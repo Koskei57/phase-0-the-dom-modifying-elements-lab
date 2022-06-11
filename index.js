7d64158aca6120caa80e628d58488ff4b81f0854
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-11 16:55:29 +0300

    commit tree a3840842608cd0f537c62209a0357650d53876ba

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ document.querySelector("main#main").remove();
 let newHeader = document.createElement("h1");
 newHeader.id = "victory";
 newHeader.innerHTML = "Victor is the champion";
-newHeader.className = "victory";
+//newHeader.className = "victory";
 const victory = document.getElementById("victory");
-victory.innerHTML =
-    "<h1>YOUR-NAME is the champion</h1>"
+victory.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
